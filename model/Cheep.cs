b4a6aa75396e9972a20a57207a2f73a71d44da0e
Tommy Nielsen <tomn@itu.dk>
2026-09-03 14:59:58 +0200

    Comment added to Cheep record.

--- model/Cheep.cs
+++ model/Cheep.cs
@@ -1,5 +1,6 @@
 namespace Model
 {
+    //Get wiser on record here: https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/record
     public record Cheep(
         string Author,
         string Message,
