3a555a713693e760759195f2bc2d4b71b9a27fe2
Tommy Nielsen <tomn@itu.dk>
2026-09-03 13:35:57 +0200

    Tidying up.

--- model/Cheep.cs
+++ model/Cheep.cs
@@ -1,16 +1,8 @@
 namespace Model
 {
     public record Cheep(
-    //{
         string Author,
         string Message,
-        long Timestamp);
-
-        /*public Cheep(string author, string message, long timestamp)
-        {
-            Author = author;
-            Message = message;
-            Timestamp = timestamp;
-        }*/
-    //}
+        long Timestamp
+        );
 }
