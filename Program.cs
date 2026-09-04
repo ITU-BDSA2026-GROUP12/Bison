bb9846b58256ce8921a2de8cd62fdcf163a9f824
Your Name <thev@itu.dk>
2026-09-04 14:31:08 +0200

    Refactored CLI argument parsing using docopt.net

--- Program.cs
+++ Program.cs
@@ -2,24 +2,34 @@
 using CsvHelper;
 using Model;
 using SimpleDB;
+using DocoptNet;
 
+//Defines rules/valid ways to use the CLI. Very whitespace sensitive here.
+const string usage = @"Bison CLI.
+
+Usage:
+  bison read
+  bison observe <message>
+
+";
+
+//parse command lines using Docopt
+var arguments = new Docopt().Apply(usage, args, version:"1.0", exit:true)!;
 
 var database = new CSVDatabase<Cheep>("bison_observe_cli_db.csv");
 
-if (args.Length == 0 || (args.Length > 0 && args[0] == "read")) {
+if (arguments["read"].IsTrue) {
     // we dont need streamreader its in the CSVDatabase class so this acts as that
     var records = database.Read();
     UserInterface.PrintObservations(records);
-
 } 
 
-else if (args.Length > 1 && args[0] == "observe") {
+else if (arguments["observe"].IsTrue) {
     // this is also refactored to use the CSVDatabase class, so we dont need to open the file here
-    string message = args[1];
+    string message = arguments["<message>"].ToString();
     string author = Environment.UserName;
     long timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
     var record = new Cheep(author, message, timestamp);
     database.Store(record);
-    
 }
