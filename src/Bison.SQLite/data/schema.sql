10460c0251f73020a86df4b0d4856bdd8fe2f6aa
Frederik <31799965+SpringyAndFriendsYT@users.noreply.github.com>
2026-09-26 11:39:09 +0200

    Added comment and proposal tables

--- src/Bison.SQLite/data/schema.sql
+++ src/Bison.SQLite/data/schema.sql
@@ -12,3 +12,25 @@ create table observation (
   text string not null,
   pub_date integer
 );
+
+drop table if exists comment;
+create table comment (
+  comment_id integer primary key autoincrement,
+  observation_id integer not null,
+  author_id integer not null,
+  text string not null,
+  pub_date integer,
+  foreign key (observation_id) references observation(observation_id),
+  foreign key (author_id) references user(user_id)
+);
+
+drop table if exists proposal;
+create table proposal (
+  proposal_id integer primary key autoincrement,
+  observation_id integer not null,
+  author_id integer not null,
+  taxon_id string not null,
+  pub_date integer,
+  foreign key (observation_id) references observation(observation_id),
+  foreign key (author_id) references user(user_id)
+);
